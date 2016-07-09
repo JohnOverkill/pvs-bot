f8b238d3cae585e8b7cd6cf5bc20d516d5b493cd
Alexander Graul <alexander.graul@posteo.de>
2016-07-09 13:12:13 +0200

    need await, read discord.py docs

--- simple_bot.py
+++ simple_bot.py
@@ -1,18 +1,13 @@
-#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import asyncio
 import discord
 client = discord.Client()
-
-
-def assign_region(message):
-    region = message.content.replace('!region', '')
-    client.send_message(message.channel, region)
 
 
 @client.async_event
 def on_message(message):
     if message.content.startswith('!region'):
-        assign_region(message)
+        region = message.content.replace('!region', '')
+        await client.send_message(message.channel, region)
 
 client.run('MTk5NDY2MzA2ODc2MDgwMTI4.CmBsyg.pcJmTTObQn-cbDZcgGWRdXTjUq8')
