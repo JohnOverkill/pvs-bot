d2c1154fa6aff1d54b9ec5509e512b825cd515c2
Alexander Graul <alexander.graul@posteo.de>
2016-07-10 01:34:57 +0200

    tried asyncio; didn't work

--- simple_bot.py
+++ simple_bot.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import asyncio
 import discord
 client = discord.Client()
 
 
+@asyncio.coroutine
 def assign_region(message):
     region = message.content.replace('!region', '')
     client.send_message(message.channel, region)
 
 
-@client.async_event
+@client.event
+@asyncio.coroutine
 def on_message(message):
     if message.content.startswith('!region'):
-        assign_region(message)
+         yield from assign_region(message)
 
 client.run('MTk5NDY2MzA2ODc2MDgwMTI4.CmBsyg.pcJmTTObQn-cbDZcgGWRdXTjUq8')
