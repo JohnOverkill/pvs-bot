87380105e29d123b0569b064339266b72178780a
Alexander Graul <alexander.graul@posteo.de>
2016-07-12 22:45:26 +0200

    can't access message.author

--- simple_bot.py
+++ simple_bot.py
@@ -4,17 +4,16 @@ import discord
 client = discord.Client()
 
 
-# @asyncio.coroutine
-# def assign_region(message):
-#    region = message.content.replace('!region', '')
+#def assign_region(message):
+#    region = message.content.strip('!region')
 #    client.send_message(message.channel, region)
 
 
 @client.event
 @asyncio.coroutine
 def on_message(message):
-    if message.content.startswith('!test'):
-#         yield from assign_region(message)
-         yield from client.send_message(message.channel, "hello")
+#    author = message.author
+    if message.content.startswith('!region'):
+         yield from client.send_message(message.channel, "Hi there")
 
 client.run('MTk5NDY2MzA2ODc2MDgwMTI4.CmBsyg.pcJmTTObQn-cbDZcgGWRdXTjUq8')
