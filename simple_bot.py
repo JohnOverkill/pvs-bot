f4e0ea1ebce4b27ef0a75fda586c83e8486041d7
Alexander Graul <alexander.graul@posteo.de>
2016-07-12 19:36:48 +0200

    simple text responding works

--- simple_bot.py
+++ simple_bot.py
@@ -4,16 +4,17 @@ import discord
 client = discord.Client()
 
 
-@asyncio.coroutine
-def assign_region(message):
-    region = message.content.replace('!region', '')
-    client.send_message(message.channel, region)
+# @asyncio.coroutine
+# def assign_region(message):
+#    region = message.content.replace('!region', '')
+#    client.send_message(message.channel, region)
 
 
 @client.event
 @asyncio.coroutine
 def on_message(message):
-    if message.content.startswith('!region'):
-         yield from assign_region(message)
+    if message.content.startswith('!test'):
+#         yield from assign_region(message)
+         yield from client.send_message(message.channel, "hello")
 
 client.run('MTk5NDY2MzA2ODc2MDgwMTI4.CmBsyg.pcJmTTObQn-cbDZcgGWRdXTjUq8')
