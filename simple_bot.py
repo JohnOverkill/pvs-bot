d2a575778d12fa28a8cd6bd7e1fd530554841c12
Alexander Graul <alexander.graul@posteo.de>
2016-07-17 19:04:17 +0200

    every server hardcoded

--- simple_bot.py
+++ simple_bot.py
@@ -4,6 +4,7 @@ import discord
 bot = discord.Client()
 
 @asyncio.coroutine
+# servers
 def add_euw(message):
     author = message.author
     role = discord.utils.get(message.server.roles, name='euw')
@@ -93,10 +94,137 @@ def remove_br(message):
     role = discord.utils.get(message.server.roles, name='br')
     yield from bot.remove_roles(author, role)
     yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+# ranks
+def add_dia(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='dia')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
 
+def remove_dia(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='dia')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+
+def add_plat(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='plat')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_plat(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='plat')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+
+def add_gold(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='gold')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_gold(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='gold')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+
+def add_silver(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='silvers')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_silver(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='silvers')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+
+def add_bronze(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='bronze')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_bronze(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='bronze')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+# positions
+def add_top(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='top')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_top(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='top')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+
+def add_jungle(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='jungle')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_jungle(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='jungle')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+
+def add_mid(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='mid')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_mid(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='mid')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+def add_adc(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='adc')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_adc(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='adc')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
+
+def add_supp(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='support')
+    yield from bot.add_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been added to {}" .format(role))
+
+def remove_supp(message):
+    author = message.author
+    role = discord.utils.get(message.server.roles, name='support')
+    yield from bot.remove_roles(author, role)
+    yield from bot.send_message(message.channel, "You have been removed from {}" .format(role))
 @bot.event
 @asyncio.coroutine
 def on_message(message):
+# servers
     if message.content.startswith('+!euw'):
         yield from add_euw(message)
 
@@ -139,7 +267,67 @@ def on_message(message):
     elif message.content.startswith('-!br'):
         yield from remove_br(message)
 
+# ranks
+    if message.content.startswith('+!dia+'):
+        yield from add_dia+(message)
 
+    elif message.content.startswith('-!dia+'):
+        yield from remove_dia+(message)
+
+    elif message.content.startswith('+!plat'):
+        yield from add_plat(message)
+
+    elif message.content.startswith('-!plat'):
+        yield from remove_plat(message)
+
+    elif message.content.startswith('+!gold'):
+        yield from add_gold(message)
+
+    elif message.content.startswith('-!gold'):
+        yield from remove_gold(message)
+
+    if message.content.startswith('+!silver'):
+        yield from add_silver(message)
+
+    elif message.content.startswith('-!silver'):
+        yield from remove_silver(message)
+
+    elif message.content.startswith('+!bronze'):
+        yield from add_bronze(message)
+
+    elif message.content.startswith('-!bronze'):
+        yield from remove_bronze(message)
+
+# postitions
+    elif message.content.startswith('+!top'):
+        yield from add_top(message)
+
+    elif message.content.startswith('-!top'):
+        yield from remove_top(message)
+
+    elif message.content.startswith('+!jungle'):
+        yield from add_jungle(message)
+
+    elif message.content.startswith('-!jungle'):
+        yield from remove_jungle(message)
+
+    elif message.content.startswith('+!mid'):
+        yield from add_mid(message)
+
+    elif message.content.startswith('-!mid'):
+        yield from remove_mid(message)
+
+    elif message.content.startswith('+!adc'):
+        yield from add_adc(message)
+
+    elif message.content.startswith('-!adc'):
+        yield from remove_adc(message)
+
+    elif message.content.startswith('+!supp'):
+        yield from add_supp(message)
+
+    elif message.content.startswith('-!supp'):
+        yield from remove_supp(message)
 
 @bot.event
 @asyncio.coroutine
@@ -149,4 +337,4 @@ def on_ready():
         print(bot.user.id)
         print('------')
 
-bot.run('MTk5NDY2MzA2ODc2MDgwMTI4.CmBsyg.pcJmTTObQn-cbDZcgGWRdXTjUq8')
+bot.run('MjA0MjczMjQ3MzAxMjcxNTUy.Cm1DyQ.9DRCligpGTcPDVRvVbS_v37y_Wk')
